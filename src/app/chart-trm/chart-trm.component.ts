570ea6dcee25018e797b9a8b96e62651c078672e
luis espejo <hola@luisespejo.com>
2024-04-05 23:24:33 -0500

    Creación del chart por segundavez

---
+++ src/app/chart-trm/chart-trm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-trm',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-trm.component.html',
+  styleUrl: './chart-trm.component.css'
+})
+export class ChartTrmComponent {
+
+}
