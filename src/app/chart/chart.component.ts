a3d4c8528057586f953296bde8188187080f606c
luis espejo <hola@luisespejo.com>
2024-04-05 21:44:34 -0500

    Creación del componente de las graficas

---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'chartTrm',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.css'
+})
+export class ChartComponent {
+
+}
