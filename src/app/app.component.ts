3ba829f60821b08cc6b1e6e13a708d1609fd3543
luis espejo <hola@luisespejo.com>
2024-04-05 21:44:17 -0500

    Importación del componente para las graficas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetDolarComponent } from './get-dolar/get-dolar.component';
+import { ChartComponent } from './chart/chart.component';
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [RouterOutlet, GetDolarComponent],
+  imports: [RouterOutlet, GetDolarComponent, ChartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TRM Colombia';
-
 }
