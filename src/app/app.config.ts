7046c2543e1801dec37834c1faa94ba0e802e6a4
luis espejo <hola@luisespejo.com>
2024-04-30 19:54:54 -0500

    Importando httpClient en las configuraciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
-import { routes } from './app.routes'
+// Estas dos importacones son necesarias para usar httpClient en el componente
+import { provideHttpClient } from '@angular/common/http';
+import { withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
