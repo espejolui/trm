c95acc6684dbd637007c74ce5b9644ad972e9b50
luis espejo <hola@luisespejo.com>
2024-04-30 19:55:59 -0500

    Importado componentes chart y calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RateComponent } from './rate/rate.component';
+import { ChartComponent } from './chart/chart.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [RouterOutlet, RateComponent],
+  imports: [RouterOutlet, RateComponent, ChartComponent, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
