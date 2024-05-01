3a45542b6ffb69bb1fefbe4c5044420f5e625a75
luis espejo <hola@luisespejo.com>
2024-04-30 19:56:55 -0500

    Creación de componente para calcular la conversión de USD a COP.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common'; // Usado para formatear valores
+import { GetDolarDataService } from '../service/get-dolar.service';
 
 @Component({
-  selector: 'app-calculator',
+  selector: 'calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
 })
 export class CalculatorComponent {
+  public dolarInput: number = 1;
+  public dolarValue!: number;
 
+  constructor(private getDolarDataService: GetDolarDataService) {
+    this.getDolarDataService.getDolarData().subscribe((data) => {
+      const today = new Date().toLocaleDateString('es-CO', {
+        timeZone: 'America/Bogota',
+      });
+      const apiDay = new Date(data[0].vigenciadesde).toLocaleDateString(
+        'es-CO',
+        { timeZone: 'America/Bogota' }
+      );
+
+      if (today !== apiDay) {
+        this.dolarValue = data[0].valor;
+      } else {
+        this.dolarValue = data[1].valor;
+      }
+    });
+  }
+
+  dolarPeso = () => {
+    const result = this.dolarInput * this.dolarValue;
+    return result;
+  };
 }
